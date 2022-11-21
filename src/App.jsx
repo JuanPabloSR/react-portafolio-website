fb41501511466cb72800e79e20c614c4ef884a51
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2022-11-21 00:01:09 -0500

    add reactga4

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-import ReactGA from "react-ga"
+import ReactGA from "react-ga4"
 
 ReactGA.initialize("G-SZXRBTBX0K");
 
